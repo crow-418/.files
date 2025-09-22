592367c999921b30dec6693cff230c9567ba02a3
crow-418 <73722884+crow-418@users.noreply.github.com>
2025-09-22 19:32:09 +0100

    feat: add my own keybinds and remove unused ones
    set the font to jetbrains mono nerd font
    
    add col_outline and replace col_cyan with col_gray5
    
    add window rule for discord
    
    change mfact to 0.5 from 0.55
    
    change mod key to super
    
    change shell of SHCMD to bash from sh

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,21 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
+// Screenshots
+static const char *flameshotcmd[] = {"flameshot", "gui", NULL};
+
+// Music Controls
+static const char *mpctoggle[] = {"mpc", "toggle", NULL};
+static const char *mpcnext[] = {"mpc", "next", NULL};
+static const char *mpcprev[] = {"mpc", "prev", NULL};
+static const char *mpcvolup[] = {"mpc", "volume", "+2", NULL};
+static const char *mpcvoldown[] = {"mpc", "volume", "-2", NULL};
+
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
-static const unsigned int snap = 32;    /* snap pixel */
+static const unsigned int snap = 0;     /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+static const char *fonts[] = {"JetBrains Mono:Regular:size=11",
+                              "monospace:size=11"};
+static const char dmenufont[] = "JetBrains Mono:Regular:size=11";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+static const char col_gray5[] = "#222222";
+static const char col_outline[] = "#24283b";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+    [SchemeSel] = {col_gray4, col_gray5, col_outline},
 };
 
 /* tagging */
@@ -28,12 +40,13 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor */
     {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
+    {"Firefox", NULL, NULL, 1 << 0, 0, -1},
+    {"discord", NULL, NULL, 1 << 2, 0, -1},
 };
 
 /* layout(s) */
-static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;    /* number of clients in master area */
+static const float mfact = 0.5; /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;   /* number of clients in master area */
 static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen =
@@ -49,7 +62,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                                      \
   {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
       {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
@@ -59,36 +72,49 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
   {                                                                            \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+    .v = (const char *[]) { "/usr/bin/bash", "-c", cmd, NULL }                 \
   }
 
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+    "dmenu_run", "-m",  dmenumon,  "-fn", dmenufont, "-nb", col_gray1, "-nf",
+    col_gray3,   "-sb", col_gray5, "-sf", col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
-    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
-    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_b, togglebar, {0}},
+    {0, 0x1008FFB2, spawn,
+     SHCMD("pactl set-source-mute @DEFAULT_SOURCE@ toggle")},
+    {MODKEY | Mod1Mask, XK_space, spawn,
+     SHCMD("$HOME/.config/scripts/repos-dmenu.sh")},
+    {0, 0x1008FF13, spawn, {.v = mpcvolup}},
+    {0, 0x1008FF11, spawn, {.v = mpcvoldown}},
+    {0, 0x1008FF16, spawn, {.v = mpcprev}},
+    {0, 0x1008FF17, spawn, {.v = mpcnext}},
+    {0, 0x1008FF14, spawn, {.v = mpctoggle}},
+    {0, 0x1008FF12, spawn, {.v = mpctoggle}},
+    {0, XK_Print, spawn, {.v = flameshotcmd}},
+    {MODKEY, XK_space, spawn, {.v = dmenucmd}},
+    {MODKEY, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY, XK_t, togglebar, {0}},
+    {MODKEY, XK_h, focusstack, {.i = -1}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
-    {MODKEY, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Return, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY | ShiftMask, XK_c, killclient, {0}},
-    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
-    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_space, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+    {MODKEY, XK_l, focusstack, {.i = +1}},
+    {MODKEY | Mod1Mask, XK_h, setmfact, {.f = -0.05}},
+    {MODKEY | Mod1Mask, XK_j, incnmaster, {.i = +1}},
+    {MODKEY | Mod1Mask, XK_k, incnmaster, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_l, setmfact, {.f = +0.05}},
+    {MODKEY, XK_z, zoom, {0}},
+    // {MODKEY, XK_Tab, view, {0}}, // what is this??
+    {MODKEY, XK_w, killclient, {0}},
+    {MODKEY | ShiftMask, XK_t, setlayout, {.v = &layouts[0]}},
+    {MODKEY | ShiftMask, XK_f, setlayout, {.v = &layouts[1]}},
+    {MODKEY | ShiftMask, XK_m, setlayout, {.v = &layouts[2]}},
+    {MODKEY | ShiftMask, XK_space, setlayout, {0}},
+    {MODKEY, XK_s, togglefloating, {0}},
     {MODKEY, XK_0, view, {.ui = ~0}},
     {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
     {MODKEY, XK_comma, focusmon, {.i = -1}},
@@ -107,13 +133,8 @@ static const Button buttons[] = {
     /* click                event mask      button          function argument */
     {ClkLtSymbol, 0, Button1, setlayout, {0}},
     {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
-    {ClkWinTitle, 0, Button2, zoom, {0}},
-    {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
     {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-    {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
     {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
     {ClkTagBar, 0, Button1, view, {0}},
     {ClkTagBar, 0, Button3, toggleview, {0}},
-    {ClkTagBar, MODKEY, Button1, tag, {0}},
-    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
 };
