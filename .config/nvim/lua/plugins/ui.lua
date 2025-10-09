03e6a1c0abc87793be50dd391af954456183bac5
crow-418 <73722884+crow-418@users.noreply.github.com>
2025-10-09 23:55:43 +0100

    refactor: remove mini.diff and which-key plugins
    I don't find them useful anymore

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,31 +1,4 @@
 return {
-    {
-        "nvim-mini/mini.diff",
-        event = "VeryLazy",
-        version = "*",
-        opts = {
-            view = {
-                style = "sign",
-                signs = {
-                    add = "┃",
-                    change = "┃",
-                    delete = "_",
-                },
-            },
-        },
-    },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-            preset = "helix",
-            delay = 0,
-            icons = {
-                mappings = false,
-            },
-            sort = { "alphanum" },
-        },
-    },
     { "j-hui/fidget.nvim", event = "LspAttach", opts = {} },
     {
         "vague2k/vague.nvim",
