f0e1a32e4fb083414b9bf6833550c5d6bfb4b0ec
crow-418 <73722884+crow-418@users.noreply.github.com>
2025-10-15 22:49:10 +0100

    fix: keeping lsp format as fallback causes unintended formatting
    if I don't have it in my conform list then I don't want a formatter to format this

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -4,7 +4,6 @@ return {
         event = "BufWritePre",
         opts = {
             format_on_save = {
-                lsp_format = "fallback",
                 timeout_ms = 500,
             },
             formatters_by_ft = {
