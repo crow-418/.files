3b5834ac141ddf509ddbe41b2da5d086b9b0a5a8
crow-418 <73722884+crow-418@users.noreply.github.com>
2025-11-27 18:22:18 +0200

    feat: set formatting to a keybind instead of happening on save

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,11 +1,18 @@
 return {
     {
         "stevearc/conform.nvim",
-        event = "BufWritePre",
-        opts = {
-            format_on_save = {
-                timeout_ms = 500,
+        keys = {
+            {
+                "<leader>fm",
+                function()
+                    require("conform").format({
+                        async = true,
+                    })
+                end,
+                desc = "Format buffer on keybind",
             },
+        },
+        opts = {
             formatters_by_ft = {
                 javascript = { "prettierd" },
                 typescript = { "prettierd" },
